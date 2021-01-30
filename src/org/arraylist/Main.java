50d7770cbfa49f322a0009720396615764a6859b
mysayamunadevi <yamunadevimysa@gmail.com>
2021-01-30 22:53:10 +0530

    initial commit

---
+++ src/org/arraylist/Main.java
@@ -0,0 +1,21 @@
+package org.arraylist;
+
+import java.util.ArrayList;
+
+public class Main {
+	
+		  public static void main(String[] args) { 
+			  ArrayList<String> fruits = new ArrayList<String>();
+		    fruits.add("Apple");
+		    fruits.add("Banana");
+		    fruits.add("Orange");
+		    fruits.add("Mango");
+		    System.out.println(fruits);
+		  }
+
+		
+		} 
+		
+
+
+	
